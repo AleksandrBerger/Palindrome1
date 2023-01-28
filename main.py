5d31b7b1954b4b66918a759cb2767f4b6c9a6f8b
AleksandrBerger <berger94@mail.ru>
2023-01-28 21:06:46 +0200

    asd

---
+++ main.py
@@ -0,0 +1,15 @@
+def is_palindrome(phrase):
+    phrase = phrase.casefold()
+    list_palindrome = list(phrase.strip(""))
+    list_original = list_palindrome.copy()
+    # print(list_original)
+    list_palindrome.reverse()
+
+    if list_palindrome == list_original:
+        print(True)
+    else:
+        print(False)
+
+
+word: str = input("Enter a word to check whether it is a palindrome: ")
+is_palindrome(word)
